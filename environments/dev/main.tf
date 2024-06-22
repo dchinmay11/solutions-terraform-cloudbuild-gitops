f37732caf56018631a5a2221f4cd070b8f3e7d71
dchinmay11 <111040548+dchinmay11@users.noreply.github.com>
2024-06-22 21:10:15 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,17 +21,7 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
 
 module "firewall" {
   source  = "../../modules/firewall"
